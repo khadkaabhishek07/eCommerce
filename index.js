1e3e61e775421043e56bd02c1003011ed57bda29
khadkaabhishek07 <khadkaabhishek749@gmail.com>
2023-10-31 21:17:37 +0545

    mondoDB connection

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
-
-app.get("/",(req,resp)=>{
-    resp.send("app is working nice. I love it")
-});
+const connectDB=async ()=>{
+    mongoose.connect('mongodb://localhost:27017/e-comm');
+    const productSchema= new mongoose.Schema({});
+    const product= mongoose.model('product',productSchema);
+    const data = await product.find();
+    console.warn(data);
+}
+connectDB();
 
 app.listen(5000)
