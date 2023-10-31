bfd8ee2434c9376d6fb48a7bdcecca0821b0d244
khadkaabhishek07 <khadkaabhishek749@gmail.com>
2023-10-31 20:05:28 +0545

    changes made to index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/",(req,resp)=>{
-    resp.send("app is working")
+    resp.send("app is working nice. I love it")
 });
 
 app.listen(5000)
